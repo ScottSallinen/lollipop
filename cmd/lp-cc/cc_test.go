66adad3d3955e0e13e7788f2697d376d7a0e493a
Chunkit <chunkitlaucont@outlook.com>
2022-09-17 16:01:45 +0800

    Implement connected components(undirected,develop)

---
+++ cmd/lp-cc/cc_test.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ScottSallinen/lollipop/enforce"
+	"github.com/ScottSallinen/lollipop/framework"
+	"github.com/ScottSallinen/lollipop/graph"
+	"github.com/ScottSallinen/lollipop/mathutils"
+)
+
+func PrintVertexProps(g *graph.Graph[VertexProperty], prefix string) {
+	top := prefix
+	sum := 0.0
+	for vidx := range g.Vertices {
+		top += fmt.Sprintf("%d:[%.3f,%.3f] ", g.Vertices[vidx].Id, g.Vertices[vidx].Property.Value, g.Vertices[vidx].Scratch)
+		sum += g.Vertices[vidx].Property.Value
+	}
+	info(top + " : " + fmt.Sprintf("%.3f", sum))
+}
+
+// Expectation when 1 is src.
+// TODO: Test other sources!
+func testGraphExpect(g *graph.Graph[VertexProperty], t *testing.T) {
+	expectations := []float64{4.0, 1.0, 3.0, 3.0, 2.0, 3.0, g.EmptyVal}
+	for i := range expectations {
+		if g.Vertices[g.VertexMap[uint32(i)]].Property.Value != expectations[i] {
+			t.Error(g.VertexMap[uint32(i)], " is ", g.Vertices[g.VertexMap[uint32(i)]].Property.Value, " expected ", expectations[i])
+		}
+	}
+}
+
+func TestAsyncStatic(t *testing.T) {
+	for tcount := 0; tcount < 100; tcount++ {
+		graph.THREADS = rand.Intn(8-1) + 1
+		g := LaunchGraphExecution("../../data/test.txt", true, false, false, false, 1)
+		PrintVertexProps(g, "")
+		testGraphExpect(g, t)
+	}
+}
+func TestSyncStatic(t *testing.T) {
+	for tcount := 0; tcount < 100; tcount++ {
+		graph.THREADS = rand.Intn(8-1) + 1
+		g := LaunchGraphExecution("../../data/test.txt", false, false, false, false, 1)
+		PrintVertexProps(g, "")
+		testGraphExpect(g, t)
+	}
+}
+func TestAsyncDynamic(t *testing.T) {
+	for tcount := 0; tcount < 100; tcount++ {
+		graph.THREADS = rand.Intn(8-1) + 1
+		g := LaunchGraphExecution("../../data/test.txt", true, true, false, false, 1)
+		testGraphExpect(g, t)
+		PrintVertexProps(g, "")
+	}
+}
+
+type StructureChange struct {
+	change graph.VisitType
+	srcRaw uint32
+	dstRaw uint32
+	weight float64
+}
+
+func DynamicGraphExecutionFromSC(sc []StructureChange, rawSrc uint32) *graph.Graph[VertexProperty] {
+	frame := framework.Framework[VertexProperty]{}
+	frame.OnInitVertex = OnInitVertex
+	frame.OnVisitVertex = OnVisitVertex
+	frame.OnFinish = OnFinish
+	frame.OnCheckCorrectness = OnCheckCorrectness
+	frame.OnEdgeAdd = OnEdgeAdd
+	frame.OnEdgeDel = OnEdgeDel
+	frame.MessageAggregator = MessageAggregator
+	frame.AggregateRetrieve = AggregateRetrieve
+
+	g := &graph.Graph[VertexProperty]{}
+	g.SourceInit = true
+	g.SourceInitVal = 1.0
+	g.EmptyVal = math.MaxFloat64
+	g.SourceVertex = rawSrc
+
+	frame.Init(g, true, true)
+
+	var feederWg sync.WaitGroup
+	feederWg.Add(1)
+	var frameWait sync.WaitGroup
+	frameWait.Add(1)
+
+	go frame.Run(g, &feederWg, &frameWait)
+
+	for _, v := range sc {
+		switch v.change {
+		case graph.ADD:
+			g.SendAdd(v.srcRaw, v.dstRaw, v.weight)
+			info("add ", v.srcRaw, v.dstRaw, v.weight)
+		case graph.DEL:
+			g.SendDel(v.srcRaw, v.dstRaw)
+			info("del ", v.srcRaw, v.dstRaw)
+		}
+	}
+
+	for i := 0; i < graph.THREADS; i++ {
+		close(g.ThreadStructureQ[i])
+	}
+	feederWg.Done()
+	frameWait.Wait()
+	return g
+}
+
+func CheckGraphStructureEquality(t *testing.T, g1 *graph.Graph[VertexProperty], g2 *graph.Graph[VertexProperty]) {
+	if len(g1.Vertices) != len(g2.Vertices) {
+		t.Error("vertex count mismatch", len(g1.Vertices), len(g2.Vertices))
+	}
+
+	for vidx := range g1.Vertices {
+		g1raw := g1.Vertices[vidx].Id
+		g2idx := g2.VertexMap[g1raw]
+
+		//g1values := &g1.Vertices[vidx].Properties
+		//g2values := &g2.Vertices[g2idx].Properties
+
+		if len(g1.Vertices[vidx].OutEdges) != len(g2.Vertices[g2idx].OutEdges) {
+			log.Println("g1:")
+			g1.PrintStructure()
+			log.Println("g2:")
+			g2.PrintStructure()
+			t.Error("edge count mismatch", len(g1.Vertices[vidx].OutEdges), len(g2.Vertices[g2idx].OutEdges))
+		}
+	}
+}
+
+func shuffleSC(sc []StructureChange) {
+	for i := range sc {
+		j := rand.Intn(i + 1)
+		sc[i], sc[j] = sc[j], sc[i]
+	}
+}
+
+func TestDynamicCreation(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	allowedVariance := float64(0.001) // ?????
+
+	testFail := false
+
+	for tcount := 0; tcount < 100; tcount++ {
+		graph.THREADS = rand.Intn(8-1) + 1
+
+		info("TestDynamicCreation ", tcount, " t ", graph.THREADS)
+
+		rawTestGraph := []StructureChange{
+			{graph.ADD, 1, 4, 1.0},
+			{graph.ADD, 2, 0, 1.0},
+			{graph.ADD, 2, 1, 1.0},
+			{graph.ADD, 3, 0, 1.0},
+			{graph.ADD, 4, 2, 1.0},
+			{graph.ADD, 4, 3, 1.0},
+			{graph.ADD, 4, 5, 1.0},
+			{graph.ADD, 6, 2, 1.0},
+		}
+		shuffleSC(rawTestGraph)
+
+		gDyn := DynamicGraphExecutionFromSC(rawTestGraph, 1)
+
+		gStatic := LaunchGraphExecution("../../data/test.txt", true, false, false, false, 1)
+
+		a := make([]float64, len(gDyn.Vertices))
+		b := make([]float64, len(gStatic.Vertices))
+
+		CheckGraphStructureEquality(t, gDyn, gStatic)
+
+		for vidx := range gDyn.Vertices {
+			g1raw := gDyn.Vertices[vidx].Id
+			g2idx := gStatic.VertexMap[g1raw]
+
+			g1values := &gDyn.Vertices[vidx]
+			g2values := &gStatic.Vertices[g2idx]
+
+			a[vidx] = g1values.Property.Value
+			b[vidx] = g2values.Property.Value
+
+			if !mathutils.FloatEquals(g1values.Property.Value, g2values.Property.Value, allowedVariance) {
+				PrintVertexProps(gStatic, "S ")
+				PrintVertexProps(gDyn, "D ")
+				t.Error("Value not equal", g1raw, g1values.Property.Value, g2values.Property.Value, "iteration", tcount)
+				testFail = true
+			}
+		}
+		enforce.ENFORCE(!testFail)
+	}
+}
---
+++ cmd/lp-cc/main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"strings"
+
+	_ "net/http/pprof"
+
+	"github.com/ScottSallinen/lollipop/enforce"
+	"github.com/ScottSallinen/lollipop/framework"
+	"github.com/ScottSallinen/lollipop/graph"
+)
+
+func info(args ...any) {
+	log.Println("[CC]\t", fmt.Sprint(args...))
+}
+
+// OnCheckCorrectness: Performs some sanity checks for correctness.
+func OnCheckCorrectness(g *graph.Graph[VertexProperty]) error {
+	// Denote vertices that claim unvisted, and ensure out edges are at least as good as we could provide
+	/*
+	for vidx := range g.Vertices {
+		ourValue := g.Vertices[vidx].Property.Value
+
+		if g.Vertices[vidx].Id == g.SourceVertex {
+			enforce.ENFORCE(ourValue == g.SourceInitVal, ourValue)
+		}
+		if ourValue == g.EmptyVal { // we were never visted
+
+		} else {
+			for eidx := range g.Vertices[vidx].OutEdges {
+				target := g.Vertices[vidx].OutEdges[eidx].Target
+				enforce.ENFORCE(g.Vertices[target].Property.Value <= ourValue)
+			}
+		}
+	}
+	*/
+	info("on check correctness: ")
+	for vidx := range g.Vertices {
+		info("# vidx = ", vidx, " Id = ", g.Vertices[vidx].Id, " value = ", g.Vertices[vidx].Property.Value)
+	}
+	return nil
+}
+
+func OracleComparison(g *graph.Graph[VertexProperty], oracle *graph.Graph[VertexProperty], resultCache *[]float64) {
+	ia := make([]float64, len(g.Vertices))
+	ib := make([]float64, len(g.Vertices))
+	numEdges := uint64(0)
+
+	for v := range g.Vertices {
+		ia[v] = oracle.Vertices[v].Property.Value
+		ib[v] = g.Vertices[v].Property.Value
+		numEdges += uint64(len(g.Vertices[v].OutEdges))
+	}
+
+	// TODO: should be parameterized...
+	const ORACLEEDGES = 28511807
+	const ORACLEVERTICES = 1791489
+
+	if resultCache == nil && numEdges == ORACLEEDGES {
+		*resultCache = make([]float64, len(ia))
+		copy(*resultCache, ia)
+	}
+	if resultCache != nil {
+		copy(ia, *resultCache)
+	}
+	info("vertexCount ", uint64(len(g.Vertices)), " edgeCount ", numEdges, " vertexPct ", (len(g.Vertices)*100)/ORACLEVERTICES, " edgePct ", (numEdges*100)/ORACLEEDGES)
+	graph.ResultCompare(ia, ib)
+
+	// Something to be done ?
+}
+
+func LaunchGraphExecution(gName string, async bool, dynamic bool, oracleRun bool, oracleFin bool, rawSrc uint32) *graph.Graph[VertexProperty] {
+	frame := framework.Framework[VertexProperty]{}
+	frame.OnInitVertex = OnInitVertex
+	frame.OnVisitVertex = OnVisitVertex
+	frame.OnFinish = OnFinish
+	frame.OnCheckCorrectness = OnCheckCorrectness
+	frame.OnEdgeAdd = OnEdgeAdd
+	frame.OnEdgeDel = OnEdgeDel
+	frame.MessageAggregator = MessageAggregator
+	frame.AggregateRetrieve = AggregateRetrieve
+	frame.OracleComparison = OracleComparison
+
+	g := &graph.Graph[VertexProperty]{}
+	// to be done ?
+	g.SourceInit = true
+	g.SourceInitVal = 1.0
+	g.EmptyVal = math.MaxFloat64
+	g.SourceVertex = rawSrc
+
+	frame.Launch(g, gName, async, dynamic, oracleRun, false)
+
+	if oracleFin {
+		frame.CompareToOracle(g)
+	}
+
+	return g
+}
+
+func main() {
+	gptr := flag.String("g", "data/test.txt", "Graph file")
+	aptr := flag.Bool("a", false, "Use async")
+	dptr := flag.Bool("d", false, "Dynamic")
+	rptr := flag.Float64("r", 0, "Use Dynamic Rate, with given rate in Edge Per Second. 0 is unbounded.")
+	optr := flag.Bool("o", false, "Compare to oracle results during runtime")
+	fptr := flag.Bool("f", false, "Compare to oracle results (computed via async) upon finishing the initial algorithm.")
+	pptr := flag.Bool("p", false, "Save vertex properties to disk")
+	iptr := flag.Int("i", 1, "Init vertex (raw id)")
+	tptr := flag.Int("t", 32, "Thread count")
+	flag.Parse()
+	gName := *gptr
+	graph.THREADS = *tptr
+	graph.TARGETRATE = *rptr
+
+	gNameMainT := strings.Split(gName, "/")
+	gNameMain := gNameMainT[len(gNameMainT)-1]
+	gNameMainTD := strings.Split(gNameMain, ".")
+	if len(gNameMainTD) > 1 {
+		gNameMain = gNameMainTD[len(gNameMainTD)-2]
+	} else {
+		gNameMain = gNameMainTD[0]
+	}
+	gNameMain = "results/" + gNameMain
+
+	//runtime.SetMutexProfileFraction(1)
+	go func() {
+		log.Println(http.ListenAndServe("0.0.0.0:6060", nil))
+	}()
+
+	g := LaunchGraphExecution(gName, *aptr, *dptr, *optr, *fptr, uint32(*iptr))
+
+	g.ComputeGraphStats(false, false)
+
+	if *pptr {
+		resName := "static"
+		if *dptr {
+			resName = "dynamic"
+		}
+		WriteVertexProps(g, gNameMain+"-props-"+resName+".txt")
+	}
+}
+
+func WriteVertexProps(g *graph.Graph[VertexProperty], fname string) {
+	f, err := os.Create(fname)
+	enforce.ENFORCE(err)
+	defer f.Close()
+	for vidx := range g.Vertices {
+		_, err := f.WriteString(fmt.Sprintf("%d %.4f\n", g.Vertices[vidx].Id, g.Vertices[vidx].Property.Value))
+		enforce.ENFORCE(err)
+	}
+}
