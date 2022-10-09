3baa689ab4f425b1cb8398d8c8d70b02dab3298a
LuuO <luuo2000@outlook.com>
2022-10-08 23:20:38 -0700

    Add more max flow tests

--- cmd/lp-maxflow-gpr/maxflow_test.go
+++ cmd/lp-maxflow-gpr/maxflow_test.go
@@ -18,6 +18,9 @@ type TestGraph struct {
 var testGraphs = [...]TestGraph{
 	{2, 0, 3, "../../data/maxflow/test-1.txt"},
 	{8, 0, 2, "../../data/maxflow/test-2.txt"},
+	{1, 0, 1, "../../data/maxflow/comment-graph-sample-merged-shuffled.txt"},
+	//{0, 5000, 10000, "../../data/maxflow/comment-graph-sample-merged-shuffled.txt"}, // takes 3889976 ms
+	{1087, 1568, 363, "../../data/maxflow/comment-graph-sample-merged-shuffled.txt"},
 }
 
 func assertEqual(t *testing.T, expected any, actual any, prefix string) {
@@ -37,7 +40,18 @@ func TestSyncStatic(t *testing.T) {
 		for ti := 0; ti < 10; ti++ {
 			graph.THREADS = rand.Intn(8-1) + 1
 			g := LaunchGraphExecution(testGraph.Filename, false, false, testGraph.Source, testGraph.Sink)
-			g.PrintVertexProperty("Sync maxflow: ")
+			maxFlow := g.Vertices[g.VertexMap[testGraph.Sink]].Property.Excess
+			assertEqual(t, testGraph.MaxFlow, maxFlow, fmt.Sprintf("Graph %s Max flow", testGraph.Filename))
+		}
+	}
+}
+
+func TestAsyncStatic(t *testing.T) {
+	for i := range testGraphs {
+		testGraph := &testGraphs[i]
+		for ti := 0; ti < 10; ti++ {
+			graph.THREADS = rand.Intn(8-1) + 1
+			g := LaunchGraphExecution(testGraph.Filename, true, false, testGraph.Source, testGraph.Sink)
 			maxFlow := g.Vertices[g.VertexMap[testGraph.Sink]].Property.Excess
 			assertEqual(t, testGraph.MaxFlow, maxFlow, fmt.Sprintf("Graph %s Max flow", testGraph.Filename))
 		}
