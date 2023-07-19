8b7e8229e129b87eb16f39bc4545e36a631ea352
Juntong Luo <LuuO2000@outlook.com>
2023-07-19 12:25:51 -0700

    Apply suggestions from code review
    Co-authored-by: Scott Sallinen <scottsallinen@gmail.com>

--- graph/run-async.go
+++ graph/run-async.go
@@ -162,6 +162,7 @@ func (gt *GraphThread[V, E, M, N]) checkCommandsAsync(epoch *bool) {
 func ConvergeAsyncThread[V VPI[V], E EPI[E], M MVI[M], N any, A Algorithm[V, E, M, N]](alg A, g *Graph[V, E, M, N], tidx uint32, wg *sync.WaitGroup) {
 	runtime.LockOSThread()
 	gt := &g.GraphThreads[tidx]
+	gt.Status = APPLY_MSG
 	algCount := 0
 	algNoCountTimes := 0
 	epoch := false
@@ -172,7 +173,6 @@ func ConvergeAsyncThread[V VPI[V], E EPI[E], M MVI[M], N any, A Algorithm[V, E, 
 			gt.checkCommandsAsync(&epoch)
 		}
 
-		gt.Status = APPLY_MSG
 		completed, algCount = ProcessMessages[V, E, M, N](alg, g, gt, true)
 		if epoch && completed {
 			gt.Status = DONE
@@ -184,6 +184,7 @@ func ConvergeAsyncThread[V VPI[V], E EPI[E], M MVI[M], N any, A Algorithm[V, E, 
 			epoch = false
 			completed = false
 			algNoCountTimes = 0
+			gt.Status = APPLY_MSG
 		} else if algCount == 0 { // Minor back off if we didn't get, and keep getting, no messages.
 			algNoCountTimes++
 			if algNoCountTimes%100 == 0 {
