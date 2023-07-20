9c3be30c4c6a4fa93dd42946de9ae47eb89208bc
LuuO <LuuO2000@outlook.com>
2023-07-19 22:23:34 -0700

    Update GR

--- cmd/lp-push-relabel/common/global-relabeling.go
+++ cmd/lp-push-relabel/common/global-relabeling.go
@@ -1,7 +1,9 @@
 package common
 
 import (
+	"fmt"
 	"sync/atomic"
+	"time"
 
 	"github.com/rs/zerolog/log"
 
@@ -9,16 +11,23 @@ import (
 )
 
 type GlobalRelabeling struct {
-	interval    atomic.Int64
-	grCount     atomic.Int64
-	nextGrCount atomic.Int64
+	LiftCountInterval atomic.Int64
+	TimeInterval      atomic.Int64
 
-	sinkId   atomic.Uint32
-	sourceId atomic.Uint32
+	NextGrLiftCount atomic.Int64
+	NextGrTime      atomic.Int64
+
+	CurrentLiftCount atomic.Int64
+
+	SinkId   atomic.Uint32
+	SourceId atomic.Uint32
+
+	Triggered atomic.Bool
 }
 
 const (
-	grAlpha       = float64(1.0)
+	grAlpha       = float64(2.0)
+	grAlphaTime   = float64(5.0)
 	grMinInterval = 128
 
 	SynchronousGlobalRelabeling = true
@@ -34,47 +43,66 @@ var (
 )
 
 func (gr *GlobalRelabeling) Reset() {
-	gr.interval.Store(grMinInterval)
-	gr.sinkId.Store(EmptyValue)
-	gr.sourceId.Store(EmptyValue)
-	gr.grCount.Store(0)
-	gr.nextGrCount.Store(gr.interval.Load())
+	gr.LiftCountInterval.Store(grMinInterval)
+	gr.TimeInterval.Store(time.Second.Milliseconds())
+	gr.NextGrLiftCount.Store(gr.LiftCountInterval.Load())
+	gr.NextGrTime.Store(time.Now().UnixMilli() + gr.TimeInterval.Load())
+	gr.CurrentLiftCount.Store(0)
+	gr.SinkId.Store(EmptyValue)
+	gr.SourceId.Store(EmptyValue)
+	gr.Triggered.Store(false)
 }
 
 func (gr *GlobalRelabeling) UpdateInterval(v int64) {
-	gr.interval.Store(utils.Max(int64(grAlpha*float64(v)), grMinInterval))
+	gr.LiftCountInterval.Store(utils.Max(int64(grAlpha*float64(v)), grMinInterval))
+}
+
+func (gr *GlobalRelabeling) UpdateTimeInterval(lastGrRuntimeMilli int64) {
+	gr.TimeInterval.Store(int64(grAlphaTime * float64(lastGrRuntimeMilli)))
 }
 
 func (gr *GlobalRelabeling) RegisterSource(id uint32) {
-	gr.sourceId.Store(id)
-	Assert(gr.interval.Load() > 0, "Invalid Global Relabeling interval")
+	gr.SourceId.Store(id)
+	Assert(gr.LiftCountInterval.Load() > 0, "Invalid Global Relabeling interval")
 }
 
 func (gr *GlobalRelabeling) RegisterSink(id uint32) {
-	gr.sinkId.Store(id)
-	Assert(gr.interval.Load() > 0, "Invalid Global Relabeling interval")
+	gr.SinkId.Store(id)
+	Assert(gr.LiftCountInterval.Load() > 0, "Invalid Global Relabeling interval")
 }
 
 func (gr *GlobalRelabeling) OnLift(sendMsg func(sinkId uint32) uint64) (sent uint64) {
 	// TODO: improve performance
-	newCount := gr.grCount.Add(1)
-	nextGrCount := gr.nextGrCount.Load()
-	if newCount >= nextGrCount {
-		gr.UpdateInterval(VertexCountHelper.GetMaxVertexCount())
-		swapped := gr.nextGrCount.CompareAndSwap(nextGrCount, newCount+gr.interval.Load())
-		if swapped {
-			log.Info().Msg("Global Relabeling is triggered")
-			// Source should always be present when lift
-			sent += sendMsg(gr.sinkId.Load())
-			sent += sendMsg(gr.sourceId.Load())
-			if SynchronousGlobalRelabeling {
-				RunSynchronousGlobalRelabel()
+	newCount := gr.CurrentLiftCount.Add(1)
+	shouldGrLiftCount, shouldGrTime := newCount > gr.NextGrLiftCount.Load(), time.Now().UnixMilli() > gr.NextGrTime.Load()
+	if shouldGrLiftCount || shouldGrTime {
+		if !gr.Triggered.Swap(true) {
+			shouldGrLiftCount, shouldGrTime := newCount > gr.NextGrLiftCount.Load(), time.Now().UnixMilli() > gr.NextGrTime.Load()
+			if shouldGrLiftCount || shouldGrTime {
+				log.Info().Msg(fmt.Sprintf("Global Relabeling is triggered shouldGrLiftCount=%v shouldGrTime=%v", shouldGrLiftCount, shouldGrTime))
+				// Source should always be present when lift
+				sent += sendMsg(gr.SinkId.Load())
+				sent += sendMsg(gr.SourceId.Load())
+				if SynchronousGlobalRelabeling {
+					RunSynchronousGlobalRelabel()
+				} else {
+					gr.GlobalRelabelingDone(time.Second.Milliseconds() * 10)
+				}
 			}
 		}
 	}
 	return
 }
 
+func (gr *GlobalRelabeling) GlobalRelabelingDone(lastGrRuntimeMilli int64) {
+	gr.UpdateInterval(VertexCountHelper.GetMaxVertexCount())
+	gr.UpdateTimeInterval(lastGrRuntimeMilli)
+
+	gr.NextGrLiftCount.Store(gr.CurrentLiftCount.Load() + gr.LiftCountInterval.Load())
+	gr.NextGrTime.Store(time.Now().UnixMilli() + gr.TimeInterval.Load())
+	gr.Triggered.Swap(false)
+}
+
 func (gr *GlobalRelabeling) GetSourceAndSinkInternalIds() (source, sink uint32) {
-	return gr.sourceId.Load(), gr.sinkId.Load()
+	return gr.SourceId.Load(), gr.SinkId.Load()
 }
