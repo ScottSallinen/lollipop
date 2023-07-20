f84f80dd2d99f828135e34e6327bc67094aadab7
LuuO <LuuO2000@outlook.com>
2023-07-20 13:23:16 -0700

    Update main.go

--- cmd/lp-push-relabel/main.go
+++ cmd/lp-push-relabel/main.go
@@ -15,7 +15,6 @@ func main() {
 	sourceId := flag.Int("S", -1, "Source vertex (raw id).")
 	sinkId := flag.Int("T", -1, "Sink vertex (raw id).")
 	initialEstimatedCount := flag.Uint("V", 30, "Initial estimated number of vertices.")
-	//implementation := flag.String("I", "agg", "Implementation. see README.md")
 	benchmark := flag.Bool("B", false, "Run benchmarks")
 	globalRelabeling := flag.Bool("G", false, "Enable global relabeling")
 	graphOptions := graph.FlagsToOptions()
@@ -31,16 +30,6 @@ func main() {
 	GlobalRelabelingEnabled = *globalRelabeling
 	VertexCountHelper.Reset(int64(*initialEstimatedCount))
 
-	//switch *implementation {
-	//case "agg":
-	//	a.RunAggH(graphOptions)
-	//case "msg-h":
-	//	b.RunMsgH(graphOptions)
-	//case "msg-a":
-	//	c.RunMsgA(graphOptions)
-	//default:
-	//	log.Fatal().Msg("Unknown implementation: " + *implementation)
-	//}
 	mf, _ := k.Run(graphOptions)
 	log.Info().Msg(fmt.Sprintf("Maximum flow is %v", mf))
 }
