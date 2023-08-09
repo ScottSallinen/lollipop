88c3f286ab708de8d1a1538c2da875450efd5cef
LuuO <luuo2000@outlook.com>
2023-08-08 21:51:11 -0700

    Add the SkipDelProb option

--- graph/stream-enact-events.go
+++ graph/stream-enact-events.go
@@ -120,6 +120,7 @@ func checkToRemit[V VPI[V], E EPI[E], M MVI[M], N any, A Algorithm[V, E, M, N]](
 // Injects expired edges into the topology event buffer. To be done after the topology event buffer has been remapped with internal source ids.
 func InjectExpired[EP EPP[E], V VPI[V], E EPI[E], M MVI[M], N any](g *Graph[V, E, M, N], gt *GraphThread[V, E, M, N], changeCount uint64, uniqueCount uint64, delOnExpire uint64) (newUniqueCount uint64) {
 	latestTime := gt.TopologyEventBuff[changeCount-1].Edge.Property.GetTimestamp() // Unless there are out of order events...?
+	skipDelProb := g.Options.SkipDelProb
 
 	if latestTime == 0 && g.warnZeroTimestamp == 0 && atomic.CompareAndSwapUint64(&g.warnZeroTimestamp, 0, 1) {
 		log.Warn().Msg("WARNING: detected a 0 for timestamp event(s). Please check -pt option.")
@@ -129,10 +130,11 @@ func InjectExpired[EP EPP[E], V VPI[V], E EPI[E], M MVI[M], N any](g *Graph[V, E
 		if gt.TopologyEventBuff[i].EventType() != ADD {
 			continue
 		}
-		// TODO: Could randomize if edges are deleted. Though, if we randomize the end time, note we would need to sort the expired edges (or use heap/pq).
-		//if rand.Float64() > 0.5 {
-		//	continue
-		//}
+		// Randomize if edges are deleted.
+		// Note: if we randomize the end time, we would need to sort the expired edges (or use heap/pq).
+		if skipDelProb > 0 && rand.Float64() < skipDelProb {
+			continue
+		}
 
 		// Adjust the copied edge.
 		futureDelete := &(gt.ExpiredEdges[(uint64(len(gt.ExpiredEdges)) - changeCount + i)].Second)
